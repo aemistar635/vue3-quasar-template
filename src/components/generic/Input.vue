63b25fcd5a65ac2b5c4801f370d76c9d2fc735bf
muhammadaamir635 <muhammad.aamir@darvis.com>
2023-03-08 16:34:50 +0500

    make common components in generic folder

---
+++ src/components/generic/Input.vue
@@ -0,0 +1,26 @@
+<template>
+  <label v-if="label" class="label">{{ label }}</label>
+  <q-input outlined v-model="text" :dense="dense" v-bind="$attrs"> </q-input>
+</template>
+<script setup>
+import { placeholder } from "@babel/types";
+
+const props = defineProps({
+  label: {
+    type: String,
+    default: "",
+  },
+  placeholder: {
+    type: String,
+    default: "",
+  },
+});
+</script>
+<style>
+.label {
+  display: block;
+  font-size: 20px;
+  font-weight: 600;
+  color: #84899c;
+}
+</style>
