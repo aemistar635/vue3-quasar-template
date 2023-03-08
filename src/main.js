9cf359c7a9cbfe5970ed7ab880aa4b1b8ef9b282
muhammadaamir635 <muhammad.aamir@darvis.com>
2023-03-08 16:35:13 +0500

    fix format of main.js file

--- src/main.js
+++ src/main.js
@@ -1,14 +1,14 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import { Quasar } from 'quasar'
-import quasarUserOptions from './quasar-user-options'
-import Input from "@/components/generic/Input.vue"
-import Button from "@/components/generic/Button.vue"
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import { Quasar } from "quasar";
+import quasarUserOptions from "./quasar-user-options";
+import Input from "@/components/generic/Input.vue";
+import Button from "@/components/generic/Button.vue";
 
 createApp(App)
-.use(Quasar, quasarUserOptions)
-.use(router)
-.component('Input',Input)
-.component('Button',Button)
-.mount('#app')
+  .use(Quasar, quasarUserOptions)
+  .use(router)
+  .component("Input", Input)
+  .component("Button", Button)
+  .mount("#app");
