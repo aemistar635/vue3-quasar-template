28934d9fadaf727a9420223795ce0a63ef544107
muhammadaamir635 <muhammad.aamir@darvis.com>
2023-03-08 13:14:53 +0500

    add poppins font family

--- src/App.vue
+++ src/App.vue
@@ -3,3 +3,6 @@
 </template>
 
 <script setup></script>
+<style lang="scss">
+@import url("@/styles/main.scss");
+</style>
--- src/main.js
+++ src/main.js
@@ -3,5 +3,12 @@ import App from './App.vue'
 import router from './router'
 import { Quasar } from 'quasar'
 import quasarUserOptions from './quasar-user-options'
+import Input from "@/components/generic/Input.vue"
+import Button from "@/components/generic/Button.vue"
 
-createApp(App).use(Quasar, quasarUserOptions).use(router).mount('#app')
+createApp(App)
+.use(Quasar, quasarUserOptions)
+.use(router)
+.component('Input',Input)
+.component('Button',Button)
+.mount('#app')
