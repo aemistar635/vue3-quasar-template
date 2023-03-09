f1b1640805fbfcf9b42ba75420f36e58be6dead6
muhammadaamir635 <muhammad.aamir@darvis.com>
2023-03-09 12:32:24 +0500

    make generic input comoponent

--- src/components/generic/Input.vue
+++ src/components/generic/Input.vue
@@ -1,10 +1,25 @@
 <template>
-  <label v-if="label" class="label">{{ label }}</label>
-  <q-input outlined v-model="text" :dense="dense" v-bind="$attrs"> </q-input>
+  <label v-if="label" class="label">Enter Your {{ label }}</label>
+  <q-input
+    v-model="text"
+    outlined
+    v-bind="$attrs"
+    :placeholder="placeholder ? placeholder : label"
+    :type="isPasswordType ? 'password' : 'text'"
+    :value="modelValue"
+    @input="$emit('update:modelValue', $event.target.value)"
+  >
+    <template v-if="type == 'password'" v-slot:append>
+      <q-icon
+        :name="isPasswordType ? 'visibility_off' : 'visibility'"
+        class="cursor-pointer"
+        @click="isPasswordType = !isPasswordType"
+      />
+    </template>
+  </q-input>
 </template>
 <script setup>
-import { placeholder } from "@babel/types";
-
+import { ref } from "vue";
 const props = defineProps({
   label: {
     type: String,
@@ -14,7 +29,16 @@ const props = defineProps({
     type: String,
     default: "",
   },
+  modelValue: {
+    type: String,
+    default: "",
+  },
+  type: {
+    type: String,
+    default: "text",
+  },
 });
+const isPasswordType = ref(props.type == "password" ? true : false);
 </script>
 <style>
 .label {
