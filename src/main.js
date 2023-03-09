10b7b9325df3ad03814b716f7b3934f8726c862a
muhammadaamir635 <muhammad.aamir@darvis.com>
2023-03-09 14:45:04 +0500

    make generic component for icon

--- src/main.js
+++ src/main.js
@@ -5,10 +5,12 @@ import { Quasar } from "quasar";
 import quasarUserOptions from "./quasar-user-options";
 import Input from "@/components/generic/Input.vue";
 import Button from "@/components/generic/Button.vue";
+import Icon from "@/components/generic/Icon.vue";
 
 createApp(App)
   .use(Quasar, quasarUserOptions)
   .use(router)
   .component("Input", Input)
   .component("Button", Button)
+  .component("Icon", Icon)
   .mount("#app");
